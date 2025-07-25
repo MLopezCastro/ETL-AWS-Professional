468dd9a90a73483422c82399cb9cb7f8daa80890
Marcelo Fabian Lopez Castro <marcelofabianlopezcastro@gmail.com>
2025-07-25 08:18:01 -0300

    v

--- ETL_Lambda_Main/scripts/aws_uploader.py
+++
@@ -1,6 +0,0 @@
-import boto3
-from pathlib import Path
-
-def upload_file_to_s3(local_path: Path, bucket_name: str, s3_key: str):
-    s3 = boto3.client("s3") # Conecta con S3 usando tus credenciales
-    s3.upload_file(str(local_path), bucket_name, s3_key) # Sube el archivo
--- ETL_Lambda_Main/scripts/step_executor.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-def execute_step_function_simulation():
-    logging.info("Simulación de Step Function iniciada.")
-    logging.info("✔️ Transformaciones completadas.")
-    logging.info("✔️ Validaciones completadas.")
-    logging.info("✔️ Exportación a Parquet completada.")
-    logging.info("✔️ Subida a S3 completada.")
-    logging.info("Simulación de Step Function finalizada.")
